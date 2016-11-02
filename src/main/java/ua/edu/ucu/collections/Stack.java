2be884a77fc94348fba8b65484fa4e3ec63dad92
KaterynaMohylevska <mohylevska@ucu.edu.ua>
2016-11-02 15:04:42 +0200

    first commit

---
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -0,0 +1,5 @@
+package ImmutableCollections.src.main.java.ua.edu.ucu.collections;
+
+public class Stack {
+    
+}
---
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
@@ -0,0 +1,5 @@
+package ImmutableCollections.src.main.java.ua.edu.ucu.collections.immutable;
+
+public class ImmutableArrayList {
+    
+}
